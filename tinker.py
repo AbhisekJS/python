e273fedace3131cac05e8c04a6fd8831f27170f5
Abhisek-Khuntia <abhisek.khuntia16@gmail.com>
2019-12-13 09:14:28 +0530

    update 2

---
+++ tinker.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+# initialize tkinter
+root = Tk()
+app = Window(root)
+
+# set window title
+root.wm_title("Tkinter window")
+
+# show window
+root.mainloop()
